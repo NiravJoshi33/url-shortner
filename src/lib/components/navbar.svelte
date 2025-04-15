050f4d69b636d4fede0fb5a221c3086b381fddf8
Nirav Joshi <niravjoshi3000@gmail.com>
2025-04-15 18:40:27 +0530

    added navbar and corrected layout height

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,59 @@
+<script>
+	import Icon from '@iconify/svelte';
+	import { siteInfo } from '$lib/data';
+	const iconSize = 32;
+</script>
+
+<nav>
+	<div class="nav-content">
+		<div class="nav-links">
+			<a
+				href={siteInfo.projectGitHub}
+				target="_blank"
+				rel="noopener noreferrer"
+				aria-label="GitHub Project"
+			>
+				<Icon icon="mdi:github" width={iconSize} height={iconSize} />
+			</a>
+			<a
+				href={siteInfo.myWebsite}
+				target="_blank"
+				rel="noopener noreferrer"
+				aria-label="Personal Website"
+			>
+				<Icon icon="mdi:web" width={iconSize} height={iconSize} />
+			</a>
+		</div>
+	</div>
+</nav>
+
+<style>
+	nav {
+		background-color: var(--bg-primary);
+		padding: 0.5rem 1rem;
+	}
+
+	.nav-content {
+		display: flex;
+		justify-content: flex-end;
+		align-items: center;
+		margin: 0 auto;
+	}
+
+	.nav-links {
+		display: flex;
+		gap: 1rem;
+	}
+
+	.nav-links a {
+		color: var(--text-primary);
+		text-decoration: none;
+		display: flex;
+		align-items: center;
+		transition: color 0.2s ease-in-out;
+	}
+
+	.nav-links a:hover {
+		color: var(--text-secondary);
+	}
+</style>
---
+++ src/lib/data.ts
@@ -0,0 +1,11 @@
+export const siteInfo = {
+	name: 'Link Shortener',
+	description: 'A simple link shortener built with SvelteKit and Pocketbase.',
+	author: 'Nirav Joshi',
+	version: '1.0.0',
+	year: new Date().getFullYear(),
+	url: 'https://url-shortner-lac-nine.vercel.app/',
+	myGitHub: 'https://github.com/NiravJoshi33',
+	myWebsite: 'https://niravjoshi.dev',
+	projectGitHub: 'https://github.com/NiravJoshi33/url-shortner'
+};
